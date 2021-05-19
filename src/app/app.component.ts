8c415cb1c23d1e8904742ce9bc19f601db85c173
Jorge Valverde Jiménez <jvalveji@ccss.sa.cr>
2021-05-19 00:37:08 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+// template: '<span>Jorge</span>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+
+
 }
